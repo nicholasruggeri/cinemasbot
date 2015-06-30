3549bdbb0fb1e6c637f9f452304c90143f82c7d9
nicholasruggeri <ruggerinicholas@gmail.com>
2015-06-30 18:20:48 +0200

    Demo

---
+++ server.js
@@ -0,0 +1,62 @@
+var express = require('express');
+var fs = require('fs');
+var request = require('request');
+var cheerio = require('cheerio');
+var app     = express();
+
+
+app.get('/cinemasbot', function(req, res){
+
+    function getQueryVariable(variable){
+        var query = req.url,
+            vars = query.split("?");
+        for (var i=0;i<vars.length;i++) {
+            var pair = vars[i].split("=");
+            if(pair[0] == variable){return pair[1];}
+        }
+        return(false);
+    }
+
+    var url = req.url,
+        location = getQueryVariable('near'),
+        googleUrl;
+
+    googleUrl = 'http://www.google.it/movies?near='+location;
+
+
+
+
+    request(googleUrl, function(error, response, html){
+
+
+        if(!error){
+            var $ = cheerio.load(html);
+            var theaters = [];
+
+            $('.theater > .desc > .name a').each(function(index){
+
+                var element = {};
+                var data = $(this);
+
+                var name = data.text(),
+                    info = data.parent().parent().find('.info').text();
+
+                element.name = name;
+                element.info = info;
+
+                theaters.push({theater: element});
+
+            });
+        }
+
+        fs.writeFile('output.json', JSON.stringify(theaters, null, 4), function(err){
+            console.log('File successfully written! - Check your project directory for the output.json file');
+        });
+
+        res.send(JSON.stringify(theaters, null, 4));
+    });
+});
+
+app.listen('8081');
+console.log('Magic happens on port 8081');
+exports = module.exports = app;
