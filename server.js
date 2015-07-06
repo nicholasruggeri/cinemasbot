5ff6ed563c336006758305e588b9a5f2fa44760c
nicholasruggeri <ruggerinicholas@gmail.com>
2015-07-06 20:46:13 +0200

    set up

--- server.js
+++ server.js
@@ -17,63 +17,54 @@
     app.post('/', function (req, res) {
 
         var chat_id = req.body.message.chat.id, // telegram chat ID
-            // text = req.body.message.text.toLowerCase(). // the text the user has written
             text = req.body.message.text, // the text the user has written
             qs = {}; // object containing the query string that will be serialized
 
-        // switch(text) {
+        switch(text) {
 
-        //     case '/start':
-        //         qs = {
-        //             reply_markup: JSON.stringify({"hide_keyboard": true}),
-        //             chat_id: chat_id,
-        //             text: "Ciao, " + req.body.message.chat.first_name + ", usa /getcinema o /getfilm per avere le informazioni che preferisci"
-        //         };
-        //         cinemasBot.sendToTelegram(qs);
-        //     break;
+            case '/start':
+                qs = {
+                    reply_markup: JSON.stringify({"hide_keyboard": true}),
+                    chat_id: chat_id,
+                    text: "Ciao, " + req.body.message.chat.first_name + ", usa /getcinema o /getfilm per avere le informazioni che preferisci"
+                };
+                cinemasBot.sendToTelegram(token, qs);
+            break;
 
-        //     case '/getcinema':
-        //         var results;
-        //         cinemasBot.getCinema('bergamo', function(theaters){
-        //             qs = {
-        //                 reply_markup: JSON.stringify({ "keyboard": theaters}),
-        //                 chat_id: chat_id,
-        //                 text: 'Ecco i risultati'
-        //             };
-        //             cinemasBot.sendToTelegram(qs);
-        //         })
-        //     break;
+            case '/getcinema':
+                var results;
+                cinemasBot.getCinema('bergamo', function(theaters){
+                    qs = {
+                        reply_markup: JSON.stringify({ "keyboard": theaters}),
+                        chat_id: chat_id,
+                        text: 'Ecco i risultati'
+                    };
+                    cinemasBot.sendToTelegram(token, qs);
+                })
+            break;
 
-        // }
-        //
-        qs = {
-            chat_id: chat_id,
-            text: "Ciao, " + req.body.message.chat.first_name + ", usa /getcinema o /getfilm per avere le informazioni che preferisci"
-        };
-        cinemasBot.sendToTelegram(token, qs);
+        }
     });
 
-    app.get('/near', function(req, res){
-        location = cinemasBot.getQueryVariable('city', req);
-        cinemasBot.getCinema(location, function(theaters){
-            console.log(theaters);
-        });
-    });
+    // app.get('/near', function(req, res){
+    //     location = cinemasBot.getQueryVariable('city', req);
+    //     cinemasBot.getCinema(location, function(theaters){
+    //         console.log(theaters);
+    //     });
+    // });
 
-    app.get('/theater', function(req, res){
-        location = cinemasBot.getQueryVariable('city', req);
-        theater = cinemasBot.getQueryVariable('theater', req);
-        getTheater(location, theater, res);
-    });
+    // app.get('/theater', function(req, res){
+    //     location = cinemasBot.getQueryVariable('city', req);
+    //     theater = cinemasBot.getQueryVariable('theater', req);
+    //     getTheater(location, theater, res);
+    // });
 
-    app.get('/movie', function(req, res){
-        location = cinemasBot.getQueryVariable('city', req);
-        theater = cinemasBot.getQueryVariable('theater', req);
-        movie = cinemasBot.getQueryVariable('movie', req);
-        getMovie(location, theater, movie, res);
-    });
-
-
+    // app.get('/movie', function(req, res){
+    //     location = cinemasBot.getQueryVariable('city', req);
+    //     theater = cinemasBot.getQueryVariable('theater', req);
+    //     movie = cinemasBot.getQueryVariable('movie', req);
+    //     getMovie(location, theater, movie, res);
+    // });
 
     app.listen(process.env.PORT);
     console.log('Magic happens on port ' + process.env.PORT);
