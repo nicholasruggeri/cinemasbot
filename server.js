8e2712fd2ff0d132f9014909151c3eae97ac6233
nicholasruggeri <ruggerinicholas@gmail.com>
2015-07-06 20:50:49 +0200

    set up

--- server.js
+++ server.js
@@ -20,30 +20,35 @@
             text = req.body.message.text, // the text the user has written
             qs = {}; // object containing the query string that will be serialized
 
-        switch(text) {
+        // switch(text) {
+        //     case '/start':
+        //         qs = {
+        //             reply_markup: JSON.stringify({"hide_keyboard": true}),
+        //             chat_id: chat_id,
+        //             text: "Ciao, " + req.body.message.chat.first_name + ", usa /getcinema o /getfilm per avere le informazioni che preferisci"
+        //         };
+        //         cinemasBot.sendToTelegram(token, qs);
+        //     break;
+        //     case '/getcinema':
+        //         var results;
+        //         cinemasBot.getCinema('bergamo', function(theaters){
+        //             qs = {
+        //                 reply_markup: JSON.stringify({ "keyboard": theaters}),
+        //                 chat_id: chat_id,
+        //                 text: 'Ecco i risultati'
+        //             };
+        //             cinemasBot.sendToTelegram(token, qs);
+        //         })
+        //     break;
+        // }
 
-            case '/start':
-                qs = {
-                    reply_markup: JSON.stringify({"hide_keyboard": true}),
-                    chat_id: chat_id,
-                    text: "Ciao, " + req.body.message.chat.first_name + ", usa /getcinema o /getfilm per avere le informazioni che preferisci"
-                };
-                cinemasBot.sendToTelegram(token, qs);
-            break;
+        qs = {
+            chat_id: chat_id,
+            text: "Ciao, " + req.body.message.chat.first_name + ", usa /getcinema o /getfilm per avere le informazioni che preferisci"
+        };
+        cinemasBot.sendToTelegram(token, qs);
 
-            case '/getcinema':
-                var results;
-                cinemasBot.getCinema('bergamo', function(theaters){
-                    qs = {
-                        reply_markup: JSON.stringify({ "keyboard": theaters}),
-                        chat_id: chat_id,
-                        text: 'Ecco i risultati'
-                    };
-                    cinemasBot.sendToTelegram(token, qs);
-                })
-            break;
 
-        }
     });
 
     // app.get('/near', function(req, res){
