49580631e41d3f9de9a70e266d288552c38bb10c
nicholasruggeri <ruggerinicholas@gmail.com>
2015-07-06 22:07:33 +0200

    set up

--- server.js
+++ server.js
@@ -7,9 +7,7 @@
         cinemasBot = require('./cinemasbot');
 
     var app = express();
-    // var token = 'bot118760525:AAFcwJxKeF7pWce47z57NcU4ONBCzR8hDbA';
     var token = process.env.TELEGRAM_TOKEN;
-
 
     app.use(bodyParser.urlencoded({ extended: false }));
     app.use(bodyParser.json());
@@ -18,8 +16,8 @@
 
         console.log('****** enter server')
 
-        var chat_id = req.body.message.chat.id, // telegram chat ID
-            text = req.body.message.text, // the text the user has written
+        var chat_id = req.body.message.chat.id,
+            text = req.body.message.text,
             qs = {}; // object containing the query string that will be serialized
 
         console.log('******* msg: ', text);
@@ -27,20 +25,18 @@
         switch(text) {
             case '/start':
                 qs = {
-                    reply_markup: JSON.stringify({"hide_keyboard": true}),
                     chat_id: chat_id,
                     text: "Ciao, " + req.body.message.chat.first_name + ", usa /getcinema o /getfilm per avere le informazioni che preferisci"
                 };
                 cinemasBot.sendToTelegram(token, qs);
             break;
             case '/reset':
-               cinemasBot.getCinema('bergamo', function(theaters){
-                    qs = {
-                        chat_id: chat_id,
-                        text: 'reset'
-                    };
-                    // cinemasBot.sendToTelegram(token, qs);
-                });
+                qs = {
+                    reply_markup: JSON.stringify({"hide_keyboard": true}),
+                    chat_id: chat_id,
+                    text: "Chiudo keyboard"
+                };
+                // cinemasBot.sendToTelegram(token, qs); commento se no mi fa sclerare :P
             break;
             case '/getcinema':
                 cinemasBot.getCinema('bergamo', function(theaters){
@@ -56,29 +52,7 @@
 
     });
 
-    // app.get('/near', function(req, res){
-    //     location = cinemasBot.getQueryVariable('city', req);
-    //     cinemasBot.getCinema(location, function(theaters){
-    //         console.log(theaters);
-    //     });
-    // });
-
-    // app.get('/theater', function(req, res){
-    //     location = cinemasBot.getQueryVariable('city', req);
-    //     theater = cinemasBot.getQueryVariable('theater', req);
-    //     getTheater(location, theater, res);
-    // });
-
-    // app.get('/movie', function(req, res){
-    //     location = cinemasBot.getQueryVariable('city', req);
-    //     theater = cinemasBot.getQueryVariable('theater', req);
-    //     movie = cinemasBot.getQueryVariable('movie', req);
-    //     getMovie(location, theater, movie, res);
-    // });
-
     app.listen(process.env.PORT);
     console.log('Magic happens on port ' + process.env.PORT);
 
 }());
-
-
