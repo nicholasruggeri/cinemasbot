9edaf149cbacdb48e0937f21b36852de9c913336
nicholasruggeri <ruggerinicholas@gmail.com>
2015-07-06 18:27:07 +0200

    set upus

--- server.js
+++ server.js
@@ -3,125 +3,12 @@
     var express = require('express'),
         bodyParser = require('body-parser'),
         request = require('request'),
-        cheerio = require('cheerio');
-        // cinemasBot = require('./cinemasbot');
+        cheerio = require('cheerio'),
+        cinemasBot = require('./cinemasbot');
 
     var app = express();
     // var token = 'bot118760525:AAFcwJxKeF7pWce47z57NcU4ONBCzR8hDbA';
     var token = process.env.TELEGRAM_TOKEN;
-
-
-
-
-    var cinemasBot = {
-
-        sendToTelegram: function(qs){
-
-            request({
-                url: 'https://api.telegram.org/' + token + '/sendMessage',
-                method: 'POST',
-                qs: qs
-            }, function (err, response, body) {
-                if (err) { console.log(err); return; }
-
-                console.log('Got response ' + response.statusCode);
-                console.log(body);
-
-                res.send();
-            });
-
-        },
-
-        getQueryVariable: function(variable, req){
-            var query = req.url,
-                vars = query.split("?");
-            for (var i=0;i<vars.length;i++) {
-                var pair = vars[i].split("=");
-                if(pair[0] == variable){return pair[1];}
-            }
-            return(false);
-        },
-
-        getCinema: function(location, callback){
-            var googleUrl = 'http://www.google.it/movies?near='+location;
-            request(googleUrl, function(error, response, html){
-                if(!error){
-                    var $ = cheerio.load(html);
-                    var theaters = [];
-                    $('.theater > .desc > .name a').each(function(index){
-                        var element = {};
-                        var data = $(this);
-                        var name = data.text(),
-                            info = data.parent().parent().find('.info').text(),
-                            link = data.attr('href');
-                        element = name;
-                        theaters.push([element]);
-                    });
-                    if (typeof callback == "function")
-                        return callback(theaters);
-                    else
-                        return theaters;
-                } else {
-                    return 'error';
-                }
-            });
-        }
-
-        // getTheater: function(location, theater, res){
-        //     var googleUrl = 'http://www.google.it/movies?near='+location;
-        //     request(googleUrl, function(error, response, html){
-        //         if(!error){
-        //             var $ = cheerio.load(html);
-        //             var movies = [];
-        //             $('.theater .desc h2.name a').each(function(index){
-        //                 var text = $(this).text()
-        //                 if (decodeURI(text) == decodeURI(theater)){
-        //                     var data = $(this);
-        //                     data.parent().parent().siblings('.showtimes').find('.movie').each(function(){
-        //                         var element = {};
-        //                         var data = $(this);
-        //                         var name = data.find('a').text();
-        //                         element.name = name;
-        //                         movies.push({film: element});
-        //                     });
-        //                 }
-        //             });
-        //         };
-        //         // res.send(JSON.stringify(movies, null, 4));
-        //     });
-        // },
-
-        // getMovie: function(location, theater, movie, res){
-        //     var googleUrl = 'http://www.google.it/movies?near='+location;
-        //     request(googleUrl, function(error, response, html){
-        //         if(!error){
-        //             var $ = cheerio.load(html);
-        //             var times = [];
-        //             $('.theater .desc h2.name a').each(function(index){
-        //                 var text = $(this).text()
-        //                 if (decodeURI(text) == decodeURI(theater)){
-        //                     var data = $(this);
-        //                     data.parent().parent().siblings('.showtimes').find('.movie').each(function(){
-        //                         var text = $(this).find('.name').text();
-        //                         if (decodeURI(text) == decodeURI(movie)){
-        //                             var data = $(this);
-        //                             var movieTimes = data.find('.times').text();
-        //                             var responseTimes = "Gli orari di " + decodeURI(movie) + " sono: " + movieTimes;
-        //                             res.send(responseTimes);
-        //                             return;
-        //                         }
-        //                     });
-        //                 }
-        //             });
-        //         };
-        //     });
-        // }
-
-    }
-
-
-
-
 
 
     app.use(bodyParser.urlencoded({ extended: false }));
