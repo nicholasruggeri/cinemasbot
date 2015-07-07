2dec194805bd6d6f2799de6ab117bee35d55bd3e
nicholasruggeri <ruggerinicholas@gmail.com>
2015-07-07 16:29:39 +0200

    set upush

--- server.js
+++ server.js
@@ -17,7 +17,7 @@ app.post('/', function (req, res) {
     var chat_id = req.body.message.chat.id,
         user_action = req.body.message.text,
         user_command = user_action.split(' ')[0],
-        user_parameter = user_action.substring(user_command.length+1, user_action.length)),
+        user_parameter = user_action.substring(user_command.length+1, user_action.length),
         qs = {}; // object containing the query string that will be serialized
 
     console.log('******* user_action: ', user_action);
