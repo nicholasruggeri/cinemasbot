a2ff7942b7f9e2007baba7b85af7e29cfa5a5b4e
nicholasruggeri <ruggerinicholas@gmail.com>
2015-07-07 12:39:45 +0200

    set up

--- server.js
+++ server.js
@@ -23,6 +23,7 @@ app.post('/', function (req, res) {
     switch(text) {
         case '/start':
             qs = {
+                reply_markup: JSON.stringify({"hide_keyboard": true}),
                 chat_id: chat_id,
                 text: "Ciao, " + req.body.message.chat.first_name
             };
@@ -31,7 +32,7 @@ app.post('/', function (req, res) {
         case '/getcinema':
             cinemasBot.getCinema('bergamo', function(theaters){
                 qs = {
-                    reply_markup: JSON.stringify({ "keyboard": theaters}),
+                    reply_markup: JSON.stringify({ "keyboard": theaters}, {"one_time_keyboard": true}),
                     chat_id: chat_id,
                     text: 'Ecco i risultati'
                 };
