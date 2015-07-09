5e79107ee5103b3fd5dc803e6f748d320bace39c
nicholasruggeri <ruggerinicholas@gmail.com>
2015-07-09 11:26:02 +0200

    set up

--- cinemasbot.js
+++ cinemasbot.js
@@ -18,16 +18,6 @@ module.exports = {
         });
     },
 
-    getQueryVariable: function(variable, req){
-        var query = req.url,
-            vars = query.split("?");
-        for (var i=0;i<vars.length;i++) {
-            var pair = vars[i].split("=");
-            if(pair[0] == variable){return pair[1];}
-        }
-        return(false);
-    },
-
     getCinema: function(location, callback){
         console.log('****** enter getCinema');
         var googleUrl = 'http://www.google.it/movies?near='+location;
